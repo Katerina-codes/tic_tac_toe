c59dd98d8b5a78b2d25f0d2d7bbd1c45352b67b6
Katerina <katerinaldg@gmail.com>
2017-07-19 14:14:22 +0100

    Asks the player for a move

--- lib/input_output.rb
+++ lib/input_output.rb
@@ -1,7 +1,15 @@
 class InputOutput
 
+  def initialize(output = $stdout)
+    @output = output
+  end
+
   def display_grid(grid)
     "\n --- --- ---\n| 1 | 2 | 3 |\n --- --- ---\n| 4 | 5 | 6 |\n --- --- ---\n| 7 | 8 | 9 |\n --- --- ---"
+  end
+
+  def ask_for_move
+    @output.puts "Time to place your mark! Please choose a number from 1 - 9"
   end
 
 end
--- spec/input_output_spec.rb
+++ spec/input_output_spec.rb
@@ -3,6 +3,7 @@ require 'input_output'
 RSpec.describe InputOutput do
 
   context "grid" do
+
     it "displays a formatted grid of numbers 1 - 9 to the user" do
      input_output = InputOutput.new
      expect(input_output.display_grid([["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])).to eq("""
@@ -14,7 +15,18 @@ RSpec.describe InputOutput do
 | 7 | 8 | 9 |
  --- --- ---""")
     end
+
   end
 
+  context "displays intructions to the player" do
+
+    it "asks the player for a move" do
+      output = StringIO.new
+      input_output = InputOutput.new(output)
+      input_output.ask_for_move
+      expect(output.string).to eq("Time to place your mark! Please choose a number from 1 - 9\n")
+    end
+
+  end
 
 end
