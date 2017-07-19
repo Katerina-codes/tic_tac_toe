c1a666d42a4360d86e911267b7a1e3e6e76489bd
Katerina <katerinaldg@gmail.com>
2017-07-19 16:54:50 +0100

    Refactored tests and renamed method argument for the grid

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,27 +2,23 @@ require 'grid'
 
 RSpec.describe Grid do
 
+  let(:grid) { Grid.new }
+  let(:unmarked_grid) { [["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]] }
+
   it "draws an array of 3 arrays with numbers 1 - 9" do
-   grid = Grid.new
    expect(grid.draw_grid).to eq([["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
   end
 
   it "marks an 'X' on the 1st position of the grid" do
-    grid = Grid.new
-    current_grid = [["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]]
-    expect(grid.place_a_move(current_grid, [0, 0])).to eq([["| X |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
+    expect(grid.place_a_move(unmarked_grid, [0, 0])).to eq([["| X |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
   end
 
   it "marks an 'X' on the 2nd position of the grid" do
-    grid = Grid.new
-    current_grid = [["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]]
-    expect(grid.place_a_move(current_grid, [0, 1])).to eq([["| 1 |", "| X |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
+    expect(grid.place_a_move(unmarked_grid, [0, 1])).to eq([["| 1 |", "| X |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
   end
 
   it "marks an 'X' on the 3rd position of the grid" do
-    grid = Grid.new
-    current_grid = [["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]]
-    expect(grid.place_a_move(current_grid, [0, 2])).to eq([["| 1 |", "| 2 |", "| X |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
+    expect(grid.place_a_move(unmarked_grid, [0, 2])).to eq([["| 1 |", "| 2 |", "| X |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
   end
 
 end
