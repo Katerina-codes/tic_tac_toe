b96981e9922bb362ff827a20e8b33724c5aa5cf5
Katerina <katerinaldg@gmail.com>
2017-07-19 13:23:27 +0100

    draws an array of arrays with numbers 1- 9

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,4 +2,8 @@ require 'grid'
 
 RSpec.describe Grid do
 
+  it "draws an array of 3 arrays with numbers 1 - 9" do
+   grid = Grid.new
+   expect(grid.draw_grid).to eq([["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
+  end
 end
