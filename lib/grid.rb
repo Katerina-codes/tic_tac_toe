8a205a455296dd234f325ee71a0bf840316aa609
Katerina <katerinaldg@gmail.com>
2017-07-19 16:11:28 +0100

    Correct test expectation for making a move on 2nd position of the grid and refactored method

--- lib/grid.rb
+++ lib/grid.rb
@@ -7,11 +7,9 @@ class Grid
   def place_a_move(current_grid, move)
     first_position = move[0]
     second_position = move[1]
-    if move == [0, 0]
+    if move == [0, 0] || move == [0, 1]
       current_grid[first_position][second_position] = "| X |"
       current_grid
-    elsif move == 2
-      [["| 1 |", "| X |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]]
     else
       [["| 1 |", "| 2 |", "| X |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]]
     end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Grid do
   it "marks an 'X' on the 2nd position of the grid" do
     grid = Grid.new
     current_grid = [["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]]
-    expect(grid.place_a_move(current_grid, 2)).to eq([["| 1 |", "| X |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
+    expect(grid.place_a_move(current_grid, [0, 1])).to eq([["| 1 |", "| X |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
   end
 
   it "marks an 'X' on the 3rd position of the grid" do
