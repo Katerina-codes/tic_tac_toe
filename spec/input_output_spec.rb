eb5382c329c93f5786ec5d74e8269638157c1c49
Katerina <katerinaldg@gmail.com>
2017-07-19 13:59:58 +0100

    Added context to make tests for different methods clearer

--- spec/input_output_spec.rb
+++ spec/input_output_spec.rb
@@ -1,9 +1,11 @@
 require 'input_output'
 
 RSpec.describe InputOutput do
-  it "displays a formatted grid of numbers 1 - 9 to the user" do
-    input_output = InputOutput.new
-    expect(input_output.display_grid([["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])).to eq("""
+
+  context "grid" do
+    it "displays a formatted grid of numbers 1 - 9 to the user" do
+     input_output = InputOutput.new
+     expect(input_output.display_grid([["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])).to eq("""
  --- --- ---
 | 1 | 2 | 3 |
  --- --- ---
@@ -11,5 +13,8 @@ RSpec.describe InputOutput do
  --- --- ---
 | 7 | 8 | 9 |
  --- --- ---""")
+    end
   end
+
+
 end
