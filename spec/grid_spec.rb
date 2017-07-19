4e629589608eb245cfc58347db6518630d7c934e
Katerina <katerinaldg@gmail.com>
2017-07-19 14:54:22 +0100

    Marks an 'X' on the first position of the grid

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Grid do
    grid = Grid.new
    expect(grid.draw_grid).to eq([["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
   end
+
+  it "marks an 'X' on the 1st position of the grid" do
+    grid = Grid.new
+    current_grid = [["| 1 |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]]
+    expect(grid.place_a_move(current_grid, 1)).to eq([["| X |", "| 2 |", "| 3 |",], ["| 4 |", "| 5 |" , "| 6 |"], ["| 7 |", "| 8 |", "| 9 |"]])
+  end
+
 end
