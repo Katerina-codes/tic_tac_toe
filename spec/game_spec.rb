8df4e1166033e08356d6e208f9b81c42705f457c
Katerina <katerinaldg@gmail.com>
2017-07-21 15:40:09 +0100

    Used let to remove duplication of validator and output variables

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,27 +4,25 @@ require 'validator'
 
 RSpec.describe Game do
 
+  let(:validator) { Validator.new }
+  let(:output) { StringIO.new }
+
   it "gets a mark choice" do
     input = StringIO.new("x")
-    output = StringIO.new
     input_output = InputOutput.new(output, input)
-    validator = Validator.new
     game = Game.new(input_output, validator)
     expect(game.get_player_mark).to eq('x')
   end
 
   it "returns mark if mark is 'x' " do
     input_output = InputOutput.new
-    validator = Validator.new
     game = Game.new(input_output, validator)
     expect(game.get_valid_mark("x")).to eq("x")
   end
 
   it "returns another mark prompt if mark is not valid" do
     input = StringIO.new("x")
-    output = StringIO.new
     input_output = InputOutput.new(output, input)
-    validator = Validator.new
     game = Game.new(input_output, validator)
     game.get_valid_mark("z")
     expect(output.string).to include("Choose your mark, type 'X' or 'O'")
