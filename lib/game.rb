f48d53a83df9e2cc8e66960033ec25ec4281e5f3
Katerina <katerinaldg@gmail.com>
2017-07-21 15:20:25 +0100

    Returns mark if mark is 'X'. Added Validator class to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,21 @@
 class Game
 
-  def initialize(input_output)
+  def initialize(input_output, validator)
     @input_output = input_output
+    @validator = validator
   end
 
   def get_player_mark
     @input_output.ask_for_mark_choice
     @input_output.get_mark_choice
+  end
+
+  def get_valid_mark(mark)
+    until @validator.mark_move_valid?(mark)
+      @input_output.ask_for_mark_choice
+      mark = @input_output.get_mark_choice
+    end
+    mark
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,6 @@
 require 'game'
+require 'input_output'
+require 'validator'
 
 RSpec.describe Game do
 
@@ -6,8 +8,16 @@ RSpec.describe Game do
     input = StringIO.new("x")
     output = StringIO.new
     input_output = InputOutput.new(output, input)
-    game = Game.new(input_output)
+    validator = Validator.new
+    game = Game.new(input_output, validator)
     expect(game.get_player_mark).to eq('x')
+  end
+
+  it "returns mark if mark is 'x' " do
+    input_output = InputOutput.new
+    validator = Validator.new
+    game = Game.new(input_output, validator)
+    expect(game.get_valid_mark("x")).to eq("x")
   end
 
 end
