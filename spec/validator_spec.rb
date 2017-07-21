bf75fa2b6e76f15d489c6b4e700f702b1de795ed
Katerina <katerinaldg@gmail.com>
2017-07-21 10:43:10 +0100

    Used let to remove duplication

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -2,33 +2,29 @@ require 'validator'
 
 RSpec.describe Validator do
 
+  let(:validator) { Validator.new }
+
   it "returns true if 1 is entered" do
-    validator = Validator.new
     expect(validator.move_valid?(1)).to eq(true)
   end
 
   it "returns false if 10 is entered" do
-    validator = Validator.new
     expect(validator.move_valid?(10)).to eq(false)
   end
 
   it "returns true if 2 is entered" do
-    validator = Validator.new
     expect(validator.move_valid?(2)).to eq(true)
   end
 
   it "returns true if move equals x" do
-    validator = Validator.new
     expect(validator.mark_move_valid?("x")).to eq(true)
   end
 
   it "returns false if move is not x" do
-    validator = Validator.new
     expect(validator.mark_move_valid?("a")).to eq(false)
   end
 
   it "returns true if move is o" do
-    validator = Validator.new
     expect(validator.mark_move_valid?("o")).to eq(true)
   end
 
