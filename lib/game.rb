5e3f6f4b5aacc8daaa4c9c8619b6931725367320
Katerina <katerinaldg@gmail.com>
2017-07-21 12:04:47 +0100

    Gets player mark

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,12 @@
 class Game
+
+  def initialize(input_output = InputOutput.new)
+    @input_output = input_output
+  end
+
+  def get_player_mark
+    @input_output.ask_for_mark_choice
+    @input_output.get_mark_choice
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,13 @@
 require 'game'
 
 RSpec.describe Game do
+
+  it "gets a mark" do
+    input = StringIO.new("x")
+    output = StringIO.new
+    input_output = InputOutput.new(output, input)
+    game = Game.new(input_output)
+    expect(game.get_player_mark).to eq('x')
+  end
+
 end
