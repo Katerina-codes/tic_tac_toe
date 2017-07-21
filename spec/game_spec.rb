51d53e4a8170b99dd16981babaf3c5e4d50ad5f6
Katerina <katerinaldg@gmail.com>
2017-07-21 15:37:21 +0100

    Returns another prompt if mark is not valid

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -20,4 +20,14 @@ RSpec.describe Game do
     expect(game.get_valid_mark("x")).to eq("x")
   end
 
+  it "returns another mark prompt if mark is not valid" do
+    input = StringIO.new("x")
+    output = StringIO.new
+    input_output = InputOutput.new(output, input)
+    validator = Validator.new
+    game = Game.new(input_output, validator)
+    game.get_valid_mark("z")
+    expect(output.string).to include("Choose your mark, type 'X' or 'O'")
+  end
+
 end
