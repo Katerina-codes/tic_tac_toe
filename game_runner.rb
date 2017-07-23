c406dfe70468a169e6ed7135f75931a474ffa37b
Katerina <katerinaldg@gmail.com>
2017-07-23 16:38:28 +0100

    Added call to the game_flow from the game runner

--- game_runner.rb
+++ game_runner.rb
@@ -2,6 +2,7 @@ require_relative 'lib/game'
 require_relative 'lib/input_output'
 require_relative 'lib/validator'
 require_relative 'lib/grid'
+require_relative 'lib/converter'
 
 new_game = Game.new(InputOutput.new, Validator.new, Grid.new, Converter.new)
-new_game.get_player_mark
+new_game.game_flow
