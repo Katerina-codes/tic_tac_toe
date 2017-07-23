791af0aa5cd59af618b5554ac6addd6bf632ea8c
Katerina <katerinaldg@gmail.com>
2017-07-23 09:59:30 +0100

    Refactored tests to remove duplicatation and unnecessary space

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -2,49 +2,44 @@ require 'converter'
 
 RSpec.describe Converter do
 
-  it "converts 1 to [0, 0]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(1)).to eq([0, 0])
-  end
+  let(:converter) { Converter.new }
 
-  it "converts 2 to [0, 1]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(2)).to eq([0, 1])
-  end
+  context "Converts move to grid position" do
+    it "converts 1 to [0, 0]" do
+      expect(converter.convert_move_number(1)).to eq([0, 0])
+    end
 
-  it "converts 3 to [0, 2]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(3)).to eq([0, 2])
-  end
+    it "converts 2 to [0, 1]" do
+      expect(converter.convert_move_number(2)).to eq([0, 1])
+    end
 
-  it "converts 4 to [1, 0]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(4)).to eq([1, 0])
-  end
+    it "converts 3 to [0, 2]" do
+      expect(converter.convert_move_number(3)).to eq([0, 2])
+    end
 
-  it "converts 5 to [1, 1]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(5)).to eq([1, 1])
-  end
+    it "converts 4 to [1, 0]" do
+      expect(converter.convert_move_number(4)).to eq([1, 0])
+    end
 
-  it "converts 6 to [1, 2]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(6)).to eq([1, 2])
-  end
+    it "converts 5 to [1, 1]" do
+      expect(converter.convert_move_number(5)).to eq([1, 1])
+    end
 
-  it "converts 7 to [2, 0]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(7)).to eq([2, 0])
-  end
+    it "converts 6 to [1, 2]" do
+      expect(converter.convert_move_number(6)).to eq([1, 2])
+    end
 
-  it "converts 8 to [2, 1]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(8)).to eq([2, 1])
-  end
+    it "converts 7 to [2, 0]" do
+      expect(converter.convert_move_number(7)).to eq([2, 0])
+    end
 
-  it "converts 9 to [2, 2]" do
-    converter = Converter.new
-    expect(converter.convert_move_number(9)).to eq([2, 2])
+    it "converts 8 to [2, 1]" do
+      expect(converter.convert_move_number(8)).to eq([2, 1])
+    end
+
+    it "converts 9 to [2, 2]" do
+      expect(converter.convert_move_number(9)).to eq([2, 2])
+    end
   end
 
 end
