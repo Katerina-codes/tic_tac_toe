aed296760e779ed9d2fd0beca633ee612a21e1b1
Katerina <katerinaldg@gmail.com>
2017-08-01 19:38:26 +0100

    Added context to tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -306,15 +306,17 @@ RSpec.describe Game do
     end
   end
 
-  it "returns true if the game has been won" do
-    game = new_game_instance(input_output, human_player)
-    expect(game.game_is_won?([["marked_move", "marked_move", "marked_move"], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]], [["marked_move", "marked_move", "marked_move"]])).to eq(true)
-  end
+  context "Tracks the game status" do
+    it "returns true if the game has been won" do
+      game = new_game_instance(input_output, human_player)
+      expect(game.game_is_won?([["marked_move", "marked_move", "marked_move"], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]], [["marked_move", "marked_move", "marked_move"]])).to eq(true)
+    end
 
-  it "returns a list of winning moves with instances of 1 replaced with marked_move" do
-    game = new_game_instance(input_output, human_player)
-    winning_moves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    expect(game.get_winning_hits(winning_moves, 1)).to eq([["marked_move", 2, 3], [4, 5, 6], [7, 8, 9], ["marked_move", 4, 7], [2, 5, 8], [3, 6, 9], ["marked_move", 5, 9], [3, 5, 7]])
+    it "returns a list of winning moves with instances of 1 replaced with marked_move" do
+      game = new_game_instance(input_output, human_player)
+      winning_moves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+      expect(game.get_winning_hits(winning_moves, 1)).to eq([["marked_move", 2, 3], [4, 5, 6], [7, 8, 9], ["marked_move", 4, 7], [2, 5, 8], [3, 6, 9], ["marked_move", 5, 9], [3, 5, 7]])
+    end
   end
 
   it "returns 'O' if p1's mark is 'X' " do
