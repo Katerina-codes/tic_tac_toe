a92e5f42a25a2d6e6d45bd78c9758bacb887f86c
Katerina <katerinaldg@gmail.com>
2017-08-01 14:09:59 +0100

    Plays 7 as the 1st move

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -4,4 +4,9 @@ class ComputerPlayer
     (1..9).to_a.sample
   end
 
+
+  def get_strategic_move(current_grid)
+    7
+  end
+
 end
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe ComputerPlayer do
     possible_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     expect(possible_moves.include?(computer_player.play_move)).to eq(true)
   end
+
+  it "plays 7 as the 1st move" do
+    computer_player = ComputerPlayer.new
+    expect(computer_player.get_strategic_move([["| 1 ", "| 2 |", " 3 |"], ["| 4 ", "| 5 |" , " 6 |"], ["| 7 ", "| 8 |", " 9 |"]])).to eq(7)
+  end
+
 end
