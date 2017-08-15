717207658d4220f7c7aa9c4e0ede342c040c0d17
Katerina <katerinaldg@gmail.com>
2017-08-15 11:20:14 +0100

    Removed repetition and call to hash by extracting logic to a new method

--- lib/grid.rb
+++ lib/grid.rb
@@ -37,46 +37,34 @@ class Grid
     count
   end
 
-  def grid(current_grid, row)
-    grid = {
-      "row one" => [current_grid[0][0], current_grid[0][1], current_grid[0][2]],
-      "row two" => [current_grid[1][0], current_grid[1][1], current_grid[1][2]],
-      "row three" => [current_grid[2][0], current_grid[2][1], current_grid[2][2]],
-      "column one" => [current_grid[0][0], current_grid[1][0], current_grid[2][0]],
-      "column two" => [current_grid[0][1], current_grid[1][1], current_grid[2][1]],
-      "column three" => [current_grid[0][2], current_grid[1][2], current_grid[2][2]],
-      "diagonal one" => [current_grid[0][0], current_grid[1][1], current_grid[2][2]],
-      "diagonal two" => [current_grid[0][2], current_grid[1][1], current_grid[2][0]]
-    }
-    grid[row]
+  def three_marks_in_a_row?(grid, array_of_value_indexes, mark)
+    count = 0
+    array_of_value_indexes.each do |x, y|
+      if grid[x][y].include?(mark)
+        count +=1
+      end
+    end
+    count == 3
   end
 
   def does_any_row_have_three_marks?(current_grid, player_mark)
-    row_one = grid(current_grid, "row one")
-    row_one_mark_count = get_marks_in_a_row(row_one, player_mark)
-    row_two = grid(current_grid, "row two")
-    row_two_mark_count = get_marks_in_a_row(row_two, player_mark)
-    row_three = grid(current_grid, "row three")
-    row_three_mark_count = get_marks_in_a_row(row_three, player_mark)
-    row_one_mark_count == 3 || row_two_mark_count == 3 || row_three_mark_count == 3
+    row_one = [[0, 0], [0, 1], [0, 2]]
+    row_two = [[1, 0], [1, 1], [1, 2]]
+    row_three = [[2, 0], [2, 1], [2, 2]]
+    three_marks_in_a_row?(current_grid, row_one, player_mark) || three_marks_in_a_row?(current_grid, row_two, player_mark) || three_marks_in_a_row?(current_grid, row_three, player_mark)
   end
 
   def does_any_column_have_three_marks?(current_grid, player_mark)
-    column_one = grid(current_grid, "column one")
-    column_one_mark_count = get_marks_in_a_row(column_one, player_mark)
-    column_two = grid(current_grid, "column two")
-    column_two_mark_count = get_marks_in_a_row(column_two, player_mark)
-    column_three = grid(current_grid, "column three")
-    column_three_mark_count = get_marks_in_a_row(column_three, player_mark)
-    column_one_mark_count == 3 || column_two_mark_count == 3 || column_three == 3
+    column_one = [[0, 0], [1, 0], [2, 0]]
+    column_two = [[0, 1], [1, 1], [2, 1]]
+    column_three = [[0, 2], [1, 2], [2, 2]]
+    three_marks_in_a_row?(current_grid, column_one, player_mark) || three_marks_in_a_row?(current_grid, column_two, player_mark) || three_marks_in_a_row?(current_grid, column_three, player_mark)
   end
 
   def does_either_diagonal_have_three_marks?(current_grid, player_mark)
-    diagonal_one = grid(current_grid, "diagonal one")
-    diagonal_one_mark_count = get_marks_in_a_row(diagonal_one, player_mark)
-    diagonal_two = grid(current_grid, "diagonal two")
-    diagonal_two_mark_count = get_marks_in_a_row(diagonal_two, player_mark)
-    diagonal_one == 3 || diagonal_two_mark_count == 3
+    diagonal_one = [[0, 0], [1, 1], [2, 2]]
+    diagonal_two = [[0, 2], [1, 1], [2, 0]]
+    three_marks_in_a_row?(current_grid, diagonal_one, player_mark) || three_marks_in_a_row?(current_grid, diagonal_two, player_mark)
   end
 
 end
