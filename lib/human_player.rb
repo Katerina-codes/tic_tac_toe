23708d1e0e6d04d3df7280315e46c47ed2c23f93
Katerina <katerinaldg@gmail.com>
2017-08-22 11:28:25 +0100

    Corrected indentation

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -10,15 +10,15 @@ class HumanPlayer
   end
 
   def get_valid_move(current_grid, player_mark, converter_instance)
-     @input_output.ask_for_move
-     move = play_move(player_mark, current_grid)
-     converted_move = converter_instance.convert_move_number(move)
-     until @grid.move_valid?(move) && @grid.move_unique?(converted_move, current_grid)
-       @input_output.display_invalid_move_error
-       move = play_move(player_mark, current_grid)
-       converted_move = converter_instance.convert_move_number(move)
-     end
-     move
-   end
+    @input_output.ask_for_move
+    move = play_move(player_mark, current_grid)
+    converted_move = converter_instance.convert_move_number(move)
+    until @grid.move_valid?(move) && @grid.move_unique?(converted_move, current_grid)
+      @input_output.display_invalid_move_error
+      move = play_move(player_mark, current_grid)
+      converted_move = converter_instance.convert_move_number(move)
+    end
+    move
+  end
 
 end
